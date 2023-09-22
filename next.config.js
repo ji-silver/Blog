11c949d732c3a411460806844f76a81e1a7b5fae
jisilver <jisilver.kim@gmail.com>
2023-09-22 15:40:04 +0900

    Test

--- next.config.js
+++ next.config.js
@@ -1,22 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   domains: ["jisilver-bucket.s3.ap-northeast-2.amazonaws.com"],
-  // },
-};
-module.exports = {
+  resolve: {
+    alias: {
+      canvas: false,
+    },
+  },
   externals: {
     "utf-8-validate": "utf-8-validate",
     bufferutil: "bufferutil",
-    canvas: "commonjs canvas",
-  },
-  module: {
-    rules: [
-      {
-        test: /canvas\.node$/,
-        use: "file-loader",
-      },
-    ],
   },
 };
 
